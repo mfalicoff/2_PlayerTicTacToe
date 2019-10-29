2698cdabb2957d9da7c788c7cbe78c0ce5dabde4
Maximiliano Falicoff <mfalicoff@yahoo.com>
2019-10-29 13:37:59 -0400

    printmap, next move init
    printmap prints current state of map with values, next move works, but need to implement 2 player functionality

--- src/tic_tac.cpp
+++ src/tic_tac.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 
 using namespace std;
-const char map[2][2] = {'O'};
+int map[3][3] = {};
 
 void printmap();
 void nextmove(int posX, int PosY);
@@ -10,26 +10,50 @@ bool isNextMovePoss(int posX, int PosY);
 
 int main(){
 
-    cout << isNextMovePoss(0, 2);    
+    nextmove(3,2);
+    //cout << map[3][2] << endl;
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++ ){
+            cout << map[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    printmap();
     
 }
 
 void printmap(){
 
-    cout << "===================" << endl;
+    cout << "=============" << endl;
     for(int i = 0; i < 3; i++){
-        cout << "|     |     |     |"  << endl;
-        cout << " ----- ----- -----" << endl;
+        cout << "|" << setw(2) << map[i][0] << setw(2) <<  "|" << setw(2) << map[i][1] << setw(2) << "|" 
+        << setw(2) << map[i][2] << setw(2) << "|" << endl;
+        cout << " --- --- ---" << endl;
     }
-    cout << "===================" << endl;
+    cout << "=============" << endl;
 
     
 }
 
-/*
+
 void nextmove(int posX, int PosY){
 
-}*/
+    int spotX = posX - 1;
+    int spotY = PosY - 1;
+    char X = 'X';
+
+    if(isNextMovePoss(posX, PosY)){
+        if(map[spotX][spotY] == 0)
+            map[spotX][spotY] = 1;
+        else
+            cout << "Error, position already occupied";
+    }else
+        cout << "Move out of boundary";
+
+
+}
 
 bool isNextMovePoss(int posX, int PosY){
     if(posX > 3 || posX <= 0 || PosY > 3 || PosY <= 0)
