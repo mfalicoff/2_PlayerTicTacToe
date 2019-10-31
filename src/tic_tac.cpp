399f0252ae5f593efe11fd397f0f7036ab61ceb5
Maximiliano Falicoff <mfalicoff@yahoo.com>
2019-10-31 16:01:33 -0400

    second player add functionality
    second player add functionality
    map changed to char

--- src/tic_tac.cpp
+++ src/tic_tac.cpp
@@ -2,55 +2,68 @@
 #include <iomanip>
 
 using namespace std;
-int map[3][3] = {};
+
+bool player; //if player == 0, then player 1 is playing
+char map[3][3] ;
 
 void printmap();
-void nextmove(int posX, int PosY);
+void nextmove(int posX, int PosY, bool player);
 bool isNextMovePoss(int posX, int PosY);
+bool isgamewon();
 
 int main(){
 
-    nextmove(3,2);
-    //cout << map[3][2] << endl;
-
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++ ){
-            cout << map[i][j] << " ";
+    //Initalizing map to underscrores
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            map[i][j] = '_';
         }
-        cout << endl;
+        
     }
-
-    printmap();
+    
+    
     
 }
 
 void printmap(){
 
     cout << "=============" << endl;
+    
     for(int i = 0; i < 3; i++){
         cout << "|" << setw(2) << map[i][0] << setw(2) <<  "|" << setw(2) << map[i][1] << setw(2) << "|" 
         << setw(2) << map[i][2] << setw(2) << "|" << endl;
-        cout << " --- --- ---" << endl;
-    }
+        if(i < 2)
+            cout << endl;
+    }    
     cout << "=============" << endl;
-
     
 }
 
 
-void nextmove(int posX, int PosY){
+void nextmove(int posX, int PosY, bool player){
 
     int spotX = posX - 1;
     int spotY = PosY - 1;
-    char X = 'X';
 
-    if(isNextMovePoss(posX, PosY)){
-        if(map[spotX][spotY] == 0)
-            map[spotX][spotY] = 1;
+    if(player){
+        if(isNextMovePoss(posX, PosY)){
+            if(map[spotX][spotY] == '_')
+                map[spotX][spotY] = 'X';
+            else
+                cout << "Error, position already occupied";
+        }else
+            cout << "Move out of boundary";
+    }else{
+        if(isNextMovePoss(posX, PosY)){
+            if(map[spotX][spotY] == '_')
+                map[spotX][spotY] = 'O';
         else
             cout << "Error, position already occupied";
-    }else
+        }else
         cout << "Move out of boundary";
+    }
+
+   
 
 
 }
@@ -61,4 +74,22 @@ bool isNextMovePoss(int posX, int PosY){
     else
         return true;
         
+}
+
+bool isgamewon(){
+
+    if((map[0][0] == map[0][1]) && (map[0][0] == map[0][2]) && (map[0][0] != '_' )) //First line across
+        return true;
+    else if((map[0][0] == map[1][0]) && (map[0][0] == map[2][0]) && (map[0][0] != '_' )) // first line down
+        return true;
+    else if ((map[0][0] == map[1][1]) && (map[0][0] == map[2][2]) && (map[0][0] != '_' )) // diagonal
+        return true;
+    else if( (map[2][0] == map[2][1]) && (map[2][0] == map[2][2]) && (map[2][0] != '_' )) //bottom line across
+        return true;
+    else if( (map[0][2] == map[1][2]) && (map[0][2] == map[2][2]) && (map[0][2] != '_' )) //line right down
+        return true;
+    else if( (map[1][0] == map[1][1]) && (map[1][0] == map[1][2]) && (map[1][0] != '_' )) //middle line
+        return true;
+    else
+        return false;
 }
